07f8c1ce6cfa130996c9e90a50bcd49f9c961be1
Thomas Beeley <tbeeley@gmail.com>
2014-08-27 15:15:07 +0100

    capacity added

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,42 +3,42 @@ require 'plane'
 
 describe 'airport' do
 
+	let(:airport)  { Airport.new("heathrow", 100) }
+	let(:plane)	   { double(:plane)		 	 	  }
+
+	def fill_airport(airport)
+		100.times { airport.accept_plane }
+	end
+
 	context 'when created' do
 
-		let(:heathrow)  { Airport.new("heathrow", 100)}
-		let(:boeing)	{ double(:plane)		 	  }
-
-
 		it 'should have no planes' do
-			expect(heathrow.plane_count).to eq 0
+			expect(airport.plane_count).to eq 0
 		end
 
 		it 'should be created with a name' do
-			expect(heathrow.name).to eq 'heathrow'
+			expect(airport.name).to eq 'heathrow'
 		end
 
 		it 'should have a custom capacity' do
-			expect(heathrow.capacity).to eq 100
+			expect(airport.capacity).to eq 100
 		end
 
 	end
 
 	context 'takeoff and landing' do
 
-		let(:heathrow)  { Airport.new("heathrow")}
-		let(:boeing)	{ double(:plane)		 }
-
 		it 'can accept a plane for landing' do
-			expect(heathrow.plane_count).to eq 0
-			heathrow.accept_plane(boeing)
-			expect(heathrow.plane_count).to eq 1
+			expect(airport.plane_count).to eq 0
+			airport.accept_plane(plane)
+			expect(airport.plane_count).to eq 1
 		end
 
 		it 'can release a plane for takeoff' do
-			heathrow.accept_plane(boeing)
-			expect(heathrow.plane_count).to eq 1
-			heathrow.release_plane(boeing)
-			expect(heathrow.plane_count).to eq 0
+			airport.accept_plane(plane)
+			expect(airport.plane_count).to eq 1
+			airport.release_plane(plane)
+			expect(airport.plane_count).to eq 0
 		end
 
 	end
